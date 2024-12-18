738189718b8de87cb7d08bd1a733323ecfb09b0a
MelisaPeano <melisa.peano@gmail.com>
2024-12-18 01:00:08 -0300

    changes

--- back/src/config/typeorm.ts
+++ back/src/config/typeorm.ts
@@ -4,14 +4,6 @@ import { DataSourceOptions } from 'typeorm';
 
 dotenvConfig({ path: '.env.development' });
 
-// const SqliteTestDataSourceOptions: DataSourceOptions = {
-//   type: 'sqlite',
-//   database: ':memory:',
-//   synchronize: true,
-//   entities: ['dist/**/*.entity{.ts,.js}'],
-//   dropSchema: true,
-// }
-
 const config: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
@@ -27,7 +19,4 @@ const config: DataSourceOptions = {
   ssl: false,
 };
 export default registerAs('typeorm', () => config);
-// export const sqliteTestDataSource = registerAs('sqlite', () => SqliteTestDataSourceOptions);
 
-// export const conectionSource = new DataSource(config);
-// export const sqliteTest = new DataSource(SqliteTestDataSourceOptions);
